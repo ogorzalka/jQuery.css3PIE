a515590932897489a8c40430d31a40002ac06c35
Olivier <og@agencecosmic.com>
2010-07-15 14:19:31 +0200

    First commit of the Jquery Plugin

---
+++ js/css3pie.plugin.js
@@ -0,0 +1,9 @@
+/* Use the spectacular CSS3 Pie (http://css3pie.com) with jQuery ! */
+;(function($) {
+   $.fn.css3PIE = function(options) {
+     var opts = $.extend({}, { htcUrl:'js/PIE.htc' }, options);  // "htcUrl" defines the path where the htc is located
+     return this.each(function(i) {
+       this.style.behavior = 'url('+opts.htcUrl+')'; // ok it's time to rock :)
+     });
+   };
+})(jQuery); // jquery goodness !!!
